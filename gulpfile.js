ca134b8428f497705551607487b25fa3133cd45b
Dilya <dilya9961@gmail.com>
2020-01-09 23:53:50 +0300

    fixed bugs

--- gulpfile.js
+++ gulpfile.js
@@ -92,7 +92,7 @@ gulp.task("server", function () {
     server: "build/"
   });
 
-  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
+  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css", "refresh"));
   gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
 });
